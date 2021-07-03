fb20585670f5a10fbaa96a412cace4b0a7c5a50c
Hemmerich Fabian <fabian.hemmerich@extern.a1.at>
2021-07-03 17:13:40 +0200

    Materialify

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script lang="ts">
-	export let name: string;
+	import { AppBar, Button, Icon, Menu, ListItem, MaterialAppMin, Tabs, Tab, } from 'svelte-materialify';
+
+	let value = 0;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<!-- TODO maybe add https://github.com/SharifClick/svelte-swipe for swiping -->
+
+<MaterialAppMin theme='dark'>
+	<AppBar>
+		<span slot="title">Weighttracker</span>
+		<div slot="extension">
+			<Tabs class="green-text" bind:value fixedTabs>
+				<div slot="tabs">
+					<Tab>Start</Tab>
+					<Tab>List</Tab>
+					<Tab>Weeks</Tab>
+				</div>
+			</Tabs>
+		</div>
+	</AppBar>
+</MaterialAppMin>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
