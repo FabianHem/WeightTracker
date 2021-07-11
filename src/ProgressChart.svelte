0aaa0ec26fd74729e448d237be249ae6e2a1eeaa
Hemmerich Fabian <fabian.hemmerich@extern.a1.at>
2021-07-11 15:05:35 +0200

    Revert "starting to mess with restdb.io"
    This reverts commit 9eaf59e064ba13e35e7c5e33aaf5d5101ea8285a.

--- src/ProgressChart.svelte
+++ src/ProgressChart.svelte
@@ -59,8 +59,8 @@
 	        axes: {
 	        	left: {
 	        	  "domain": [
-	        	    min - 3,
-	        	    max + 3
+	        	    min - 15,
+	        	    max + 15
 	        	    ]
 	        	},
 	        	bottom: {
